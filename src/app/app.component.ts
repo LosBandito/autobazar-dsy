451afe88b951f5f22445486738e87cf283b7695d
igor <prekop.igor@gmail.com>
2023-03-01 10:33:40 +0100

    modals working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import data from '../data/data.json';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor() {
+    this.modalStates = new Array(this.data.length).fill(false);
+  }
+
   title = 'AngularDalsieTesty';
+  data = data;
+
+
+  displayStyle = "none";
+  modalStates: boolean[];
+
+  ngOnInit(): void {
+  }
+
+  openPopup(i: number) {
+    this.modalStates[i] = true;
+  }
+
+  closePopup(i: number) {
+    this.modalStates[i] = false;
+
+  }
 }
+
+
