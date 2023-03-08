ee809fcc3e5ac2f4a79f1ed5e6d9d1ed4a788c48
igor <prekop.igor@gmail.com>
2023-03-08 10:48:59 +0100

    querry requests working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import data from '../data/data.json';
+import {Router} from "@angular/router";
 
 
 
@@ -9,28 +10,36 @@ import data from '../data/data.json';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor() {
-    this.modalStates = new Array(this.data.length).fill(false);
-  }
 
-  title = 'AngularDalsieTesty';
-  data = data;
+    constructor(private router: Router) {
+      this.modalStates = new Array(this.data.length).fill(false);
+    }
+    title = 'AngularDalsieTesty';
+    data = data;
 
 
-  displayStyle = "none";
-  modalStates: boolean[];
+    displayStyle = "none";
+    modalStates: boolean[];
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+    redTest(){
+      this.router.navigate(["test"])
+    }
+    openPopup(i: number) {
+      this.modalStates[i] = true;
+    }
 
-  openPopup(i: number) {
-    this.modalStates[i] = true;
-  }
+    closePopup(i: number) {
+      this.modalStates[i] = false;
 
-  closePopup(i: number) {
-    this.modalStates[i] = false;
+    }
 
-  }
+    buyItem(item: any) {
+      console.log("Click on item")
+      this.router.navigate(['buy'], { queryParams: { name: item[0], price: item[1] } });    }
+
+
 }
 
 
