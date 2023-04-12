d2a473f386d2608b6a6f85868397f1ace8a344bb
igor <prekop.igor@gmail.com>
2023-04-12 10:44:01 +0200

    merge to main,navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import data from '../data/data.json';
-import {Router} from "@angular/router";
 
 
 
@@ -10,35 +8,6 @@ import {Router} from "@angular/router";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-    constructor(private router: Router) {
-      this.modalStates = new Array(this.data.length).fill(false);
-    }
-    title = 'AngularDalsieTesty';
-    data = data;
-
-
-    displayStyle = "none";
-    modalStates: boolean[];
-
-    ngOnInit(): void {
-    }
-    redTest(){
-      this.router.navigate(["test"])
-    }
-    openPopup(i: number) {
-      this.modalStates[i] = true;
-    }
-
-    closePopup(i: number) {
-      this.modalStates[i] = false;
-
-    }
-
-    buyItem(item: any) {
-      console.log("Click on item")
-      this.router.navigate(['buy'], { queryParams: { name: item[0], price: item[1] } });    }
-
 
 }
 
